d019e703d27cedab1d1da600f81bdb596340e4fe
Ho Huynh Nga <hnga3511@gmail.com>
2020-03-27 09:55:09 +0700

    Added Ucln

--- NetExericise/NetExericise/Basic/Exercise01.cs
+++ NetExericise/NetExericise/Basic/Exercise01.cs
@@ -8,6 +8,21 @@ namespace NetExericise.Basic
 {
     public class Exercise01
     {
+        public int Ucln(int a, int b)
+        {
+            while(a * b > 0)
+            {
+                if(a > b)
+                {
+                    a %= b;
+                }
+                else
+                {
+                    b %= a;
+                }
+            }
 
+            return a + b;
+        }
     }
 }
