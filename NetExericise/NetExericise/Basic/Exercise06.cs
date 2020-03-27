f55e3b439f3e48afd27c7073bec7df5ff9ef9fdd
Ho Huynh Nga <hnga3511@gmail.com>
2020-03-27 15:08:17 +0700

    Added exercise06

---
+++ NetExericise/NetExericise/Basic/Exercise06.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExericise.Basic
+{
+    public class Exercise06
+    {
+        public int Calculate(int n)
+        {
+            return (n * (n + 1) * (n + 2)) / 3;
+        }
+    }
+}
