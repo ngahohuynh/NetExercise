a06c32ca96ce0c177d5c65d004f82d3981e105f5
Ho Huynh Nga <hnga3511@gmail.com>
2020-03-27 10:46:30 +0700

    Added exercise03

---
+++ NetExericise/NetExercise.Test/Basic/Exercise03Test.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetExericise.Basic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExercise.Test.Basic
+{
+    [TestClass]
+    public class Exercise03Test
+    {
+        [TestMethod]
+        public void Test_PrimeFactors()
+        {
+            var exercise03 = new Exercise03();
+            Assert.AreEqual(exercise03.PrimeFactors(600), "2 * 2 * 2 * 3 * 5 * 5");
+        }
+
+        [TestMethod]
+        public void Test_PrimeFactors_PrimeNumbers()
+        {
+            var exercise03 = new Exercise03();
+            Assert.AreEqual(exercise03.PrimeFactors(8191), "8191");
+        }
+    }
+}
---
+++ NetExericise/NetExericise/Basic/Exercise03.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExericise.Basic
+{
+    public class Exercise03
+    {
+        public string PrimeFactors(int n)
+        {
+            if (IsPrime(n)) return n.ToString();
+
+            StringBuilder sb = new StringBuilder();
+            int i = 2;
+            while(n > 1)
+            {
+                if (n % i == 0 && IsPrime(i))
+                {
+                    sb.Append(i + " * ");
+                    n /= i;
+                }
+                else i++;
+            }
+            sb.Remove(sb.Length - 3, 3);
+
+            return sb.ToString();
+        }
+
+        bool IsPrime(int n)
+        {
+            if (n == 0) return false;
+            if (n == 1 || n == 2) return true;
+            for(int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
