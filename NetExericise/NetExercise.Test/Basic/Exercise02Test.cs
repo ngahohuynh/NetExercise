5e9814b8476bf8fa7e308e732dc33b98a401114e
Ho Huynh Nga <hnga3511@gmail.com>
2020-03-27 10:22:03 +0700

    Added exercise02

---
+++ NetExericise/NetExercise.Test/Basic/Exercise02Test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetExericise.Basic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExercise.Test.Basic
+{
+    [TestClass]
+    public class Exercise02Test
+    {
+        [TestMethod]
+        public void Test_SumOfDigits()
+        {
+            var exercise02 = new Exercise02();
+            Assert.AreEqual(exercise02.SumOfDigits(2324142), 18);
+            Assert.AreEqual(exercise02.SumOfDigits(8799), 33);
+        }
+    }
+}
