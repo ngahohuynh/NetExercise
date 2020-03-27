7d5be1c5776790d0053de3975afea108e70716e9
Ho Huynh Nga <hnga3511@gmail.com>
2020-03-27 16:46:57 +0700

    Added exercise08

---
+++ NetExericise/NetExercise.Test/Basic/Exercise08Test.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetExericise.Basic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExercise.Test.Basic
+{
+    [TestClass]
+    public class Exercise08Test
+    {
+        [TestMethod]
+        public void Test_CalculatePi()
+        {
+            var exercise08 = new Exercise08();
+            Assert.AreEqual(exercise08.CalculatePi(0.0001f), Math.PI, 0.0001);
+        }
+    }
+}
