12ac38a65e29814aef9ece08d84d55b27ca4efe8
Ho Huynh Nga <hnga3511@gmail.com>
2020-03-27 11:57:11 +0700

    Added exercise04

--- NetExericise/NetExercise.Test/Basic/Exercise04Test.cs
+++ NetExericise/NetExercise.Test/Basic/Exercise04Test.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetExericise.Basic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,15 @@ using System.Threading.Tasks;
 
 namespace NetExercise.Test.Basic
 {
-    class Exercise04Test
+    [TestClass]
+    public class Exercise04Test
     {
+        [TestMethod]
+        public void Test_FiboNacciLessThanN()
+        {
+            var exercise04 = new Exercise04();
+            Assert.AreEqual(exercise04.FiboNacciLessThanN(15), "1 1 2 3 5 8 13");
+            Assert.AreEqual(exercise04.FiboNacciLessThanN(200), "1 1 2 3 5 8 13 21 34 55 89 144");
+        }
     }
 }
--- NetExericise/NetExericise/Basic/Exercise04.cs
+++ NetExericise/NetExericise/Basic/Exercise04.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace NetExericise.Basic
 {
-    class Exercise04
+    public class Exercise04
     {
+        public string FiboNacciLessThanN(int n)
+        {
+            List<int> fiboList = new List<int>() { 1, 1 };
+
+            int len = fiboList.Count();
+            int sum = fiboList[len - 2] + fiboList[len - 1];
+
+            while(sum <= n)
+            {
+                fiboList.Add(sum);
+                len++;
+                sum = fiboList[len - 2] + fiboList[len - 1];
+            }
+
+            return string.Join(" ", fiboList);
+        }
     }
 }
