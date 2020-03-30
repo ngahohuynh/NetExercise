79c4f81995164e751adbd95c490987c5bb2c4aaa
Ho Huynh Nga <hnga3511@gmail.com>
2020-03-30 10:03:40 +0700

    Added exercise11

---
+++ NetExericise/NetExericise/Basic/Exercise11.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExericise.Basic
+{
+    public class Exercise11
+    {
+        public int ReverseBits(int n)
+        {
+            var m = 0;
+
+            while (n > 0)
+            {
+                m <<= 1;
+                if ((int)(n & 1) == 1)
+                {
+                    m ^= 1;
+                }
+                n >>= 1;
+            }
+
+            return m;
+        }
+    }
+}
