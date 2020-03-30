1b959c708cb9c98706b84dac5c88a6dc0277f8f7
Ho Huynh Nga <hnga3511@gmail.com>
2020-03-30 17:57:40 +0700

    Added exercise01

---
+++ NetExericise/NetExericise/Array/Exercise01.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExericise.Array
+{
+    public class Exercise01
+    {
+        public string NumbersAppearTwice(int[] arr)
+        {
+            var dict = new Dictionary<int, int>();
+            var result = new List<int>();
+
+            foreach (var num in arr)
+            {
+                if (!dict.ContainsKey(num))
+                    dict.Add(num, 1);
+
+                else if (dict[num] == 1)
+                    dict[num]++;
+
+                else
+                {
+                    if (!result.Contains(num))
+                        result.Add(num);
+                }
+            }
+
+            return string.Join(" ", result);
+        }
+    }
+}
