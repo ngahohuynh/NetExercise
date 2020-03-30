8a5f46565de980bc92fac2c2fce4311ad98aec71
Ho Huynh Nga <hnga3511@gmail.com>
2020-03-30 15:16:52 +0700

    Added exercise16

---
+++ NetExericise/NetExercise.Test/Basic/Exercise16Test.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetExericise.Basic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExercise.Test.Basic
+{
+    [TestClass]
+    public class Exercise16Test
+    {
+        [TestMethod]
+        public void Test_CheckNumber()
+        {
+            var exercise16 = new Exercise16();
+            Assert.AreEqual(exercise16.CheckNumber(15), true);
+        }
+    }
+}
