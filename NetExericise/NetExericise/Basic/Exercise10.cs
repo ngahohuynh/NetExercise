133d5ab0569a716e61cb0af863780c9b583ed9d8
Ho Huynh Nga <hnga3511@gmail.com>
2020-03-30 17:23:03 +0700

    Refactor

--- NetExericise/NetExericise/Basic/Exercise10.cs
+++ NetExericise/NetExericise/Basic/Exercise10.cs
@@ -12,34 +12,17 @@ namespace NetExericise.Basic
         {
             if (n == 1) return 1;
 
-            double left, right, mid;
-
-            if (n < 1)
-            {
-                left = 0;
-                right = 1;
-            }
-
-            else
-            {
-                left = 0;
-                right = n;
-            }
+            var left = 0d;
+            var right = (n < 1) ? 1d : n;
 
             do
             {
-                mid = (left + right) / 2;
-                if (Func(mid, n) == 0) break;
-                if (Func(left, n) * Func(mid, n) < 0) right = mid;
+                var mid = (left + right) / 2;
+                if (mid * mid * mid > n) right = mid;
                 else left = mid;
-            } while ((left - right) * (left - right) > e * e);
+            } while (right - left > e);
 
-            return mid;
-        }
-
-        double Func(double x, double n)
-        {
-            return x * x * x - n;
+            return (left + right) / 2;
         }
     }
 }
