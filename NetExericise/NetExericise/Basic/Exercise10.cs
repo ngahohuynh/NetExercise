32ea8b82dafa80cb22d583c5dd25e799a9f103de
Ho Huynh Nga <hnga3511@gmail.com>
2020-03-30 09:53:29 +0700

    Added exercise10

---
+++ NetExericise/NetExericise/Basic/Exercise10.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExericise.Basic
+{
+    public class Exercise10
+    {
+        public double CalculateCubeRoot(double n, double e)
+        {
+            if (n == 1) return 1;
+
+            double left, right, mid;
+
+            if (n < 1)
+            {
+                left = 0;
+                right = 1;
+            }
+
+            else
+            {
+                left = 0;
+                right = n;
+            }
+
+            do
+            {
+                mid = (left + right) / 2;
+                if (Func(mid, n) == 0) break;
+                if (Func(left, n) * Func(mid, n) < 0) right = mid;
+                else left = mid;
+            } while ((left - right) * (left - right) > e * e);
+
+            return mid;
+        }
+
+        double Func(double x, double n)
+        {
+            return x * x * x - n;
+        }
+    }
+}
