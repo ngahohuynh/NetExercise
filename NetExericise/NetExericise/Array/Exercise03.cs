dec753ffb0329021f7acc49d693e7dcebe3bbf1a
Ho Huynh Nga <hnga3511@gmail.com>
2020-03-31 10:53:39 +0700

    Added exercise03

---
+++ NetExericise/NetExericise/Array/Exercise03.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExericise.Array
+{
+    public class Exercise03
+    {
+        public int OddEvenDifference(int[] arr)
+        {
+            return arr.Sum(i =>
+            {
+                if (i % 2 == 0)
+                {
+                    return i;
+                }
+                else
+                {
+                    return -i;
+                }
+            });
+        }
+    }
+}
