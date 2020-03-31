cf7ca6290b7be33884524be2032511506e46c435
Ho Huynh Nga <hnga3511@gmail.com>
2020-03-31 14:02:59 +0700

    Added exercise07

--- NetExericise/NetExercise.Test/Array/Exercise07Test.cs
+++ NetExericise/NetExercise.Test/Array/Exercise07Test.cs
@@ -15,6 +15,11 @@ namespace NetExercise.Test.Array
         public void Test_ChunkArray()
         {
             var exercise07 = new Exercise07();
+            var testResult = exercise07.ChunkArray(new int[] { 5, 6, 3, 5, 7, 8, 9, 1, 2 }, 4);
+
+            Assert.AreEqual(testResult.Count(), 3);
+            CollectionAssert.AreEqual(testResult[0], new List<int> { 5, 6, 3, 5 });
+            CollectionAssert.AreEqual(testResult[2], new List<int> { 2 });
         }
     }
 }
