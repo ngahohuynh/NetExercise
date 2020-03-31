bcb93a1be8fb7bac495df6fdfc4e896b5e6f7e15
Ho Huynh Nga <hnga3511@gmail.com>
2020-03-31 10:58:14 +0700

    Added exercise04

---
+++ NetExericise/NetExericise/Array/Exercise04.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExericise.Array
+{
+    public class Exercise04
+    {
+        public int SumOfPrimeNumbers(int[] arr)
+        {
+            return arr.Sum(i => {
+                if (IsPrime(i))
+                {
+                    return i;
+                }
+                return 0;
+            });
+        }
+
+        bool IsPrime(int n)
+        {
+            if (n <= 1) return false;
+
+            for (var i = 2; i * i <= n; i++)
+            {
+                if (n % i == 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
