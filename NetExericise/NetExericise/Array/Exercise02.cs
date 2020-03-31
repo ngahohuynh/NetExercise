3441dc406c838951c1b270a50f774cd6176c9677
Ho Huynh Nga <hnga3511@gmail.com>
2020-03-31 10:01:48 +0700

    Added exercise02

---
+++ NetExericise/NetExericise/Array/Exercise02.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExericise.Array
+{
+    public class Exercise02
+    {
+        public int MaxTripletSum(int[] arr)
+        {
+            var sortedArr = arr.OrderByDescending(num => num).ToArray();
+            return sortedArr[0] + sortedArr[1] + sortedArr[2];
+        }
+    }
+}
