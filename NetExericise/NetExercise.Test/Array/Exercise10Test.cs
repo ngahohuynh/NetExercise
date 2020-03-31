93481f99a508a0372eb34e734f8e39f72f6f4568
Ho Huynh Nga <hnga3511@gmail.com>
2020-03-31 15:20:12 +0700

    Added exercise10

---
+++ NetExericise/NetExercise.Test/Array/Exercise10Test.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetExericise.Array;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExercise.Test.Array
+{
+    [TestClass]
+    public class Exercise10Test
+    {
+        [TestMethod]
+        public void Test_SumOfSquareNumbers()
+        {
+            var exercise10 = new Exercise10();
+            Assert.AreEqual(exercise10.SumOfSquareNumbers(new int[] { 1, 2, 5, 20, 16, 35, 36 }), 53);
+        }
+    }
+}
---
+++ NetExericise/NetExericise/Array/Exercise10.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExericise.Array
+{
+    public class Exercise10
+    {
+        public int SumOfSquareNumbers(int[] arr)
+        {
+            return arr.Where(x => IsPerfectSquare(x)).Sum();
+        }
+
+        bool IsPerfectSquare(int x)
+        {
+            return Math.Sqrt(x) - Math.Floor(Math.Sqrt(x)) == 0;
+        }
+    }
+}
