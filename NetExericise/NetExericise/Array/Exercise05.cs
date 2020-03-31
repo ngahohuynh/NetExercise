1f37aaef15c56178164801b76c66b5528aa1d6de
Ho Huynh Nga <hnga3511@gmail.com>
2020-03-31 11:03:18 +0700

    Added exercise05

---
+++ NetExericise/NetExericise/Array/Exercise05.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExericise.Array
+{
+    public class Exercise05
+    {
+        public string LongestIncreasingSubArray(int[] arr)
+        {
+            var result = new List<int>();
+            var len = 1;
+            var max = 1;
+            var index = 0;
+
+            for (var i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] >= arr[i - 1])
+                    len++;
+                else
+                {
+                    if (max < len)
+                    {
+                        max = len;
+                        index = i - max;
+                    }
+                    len = 1;
+                }
+            }
+
+            if (max < len)
+            {
+                max = len;
+                index = arr.Length - max;
+            }
+
+            for (var i = index; i < index + max; i++)
+            {
+                result.Add(arr[i]);
+            }
+
+
+            return string.Join(" ", result);
+        }
+    }
+}
