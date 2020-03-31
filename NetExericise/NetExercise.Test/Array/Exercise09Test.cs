29952c5ce59d09e558b72c53739aff4587c5abb2
Ho Huynh Nga <hnga3511@gmail.com>
2020-03-31 14:38:30 +0700

    Added exercise09

---
+++ NetExericise/NetExercise.Test/Array/Exercise09Test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetExericise.Array;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExercise.Test.Array
+{
+    [TestClass]
+    public class Exercise09Test
+    {
+        [TestMethod]
+        public void Test_GenerateIncreasingArray()
+        {
+            var exercise09 = new Exercise09();
+            var list = exercise09.GenerateIncreasingArray(20);
+            Assert.IsTrue(list.All(x => x >= 1 && x <= 20));
+        }
+    }
+}
---
+++ NetExericise/NetExericise/Array/Exercise09.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExericise.Array
+{
+    public class Exercise09
+    {
+        public List<int> GenerateIncreasingArray(int n)
+        {
+            var rand = new Random();
+            var result = new List<int>();
+
+            for(var i = 0; i < n; i++)
+            {
+                result.Add(rand.Next(1, n + 1));
+            }
+
+            result.OrderBy(x => x);
+
+            return result;
+        }
+    }
+}
