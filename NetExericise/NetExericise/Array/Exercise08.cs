284d0ac561d249483aba548f39a36534af39ecbb
Ho Huynh Nga <hnga3511@gmail.com>
2020-03-31 14:16:48 +0700

    WIP

---
+++ NetExericise/NetExericise/Array/Exercise08.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExericise.Array
+{
+    public class Exercise08
+    {
+        public int CountNumbers(int[] arr)
+        {
+            //return arr.Count(x => x % 3 == 0 && x % 5 != 0);
+            return arr.Where(x => x % 3 == 0 && x % 5 != 0).Count();
+        }
+    }
+}
