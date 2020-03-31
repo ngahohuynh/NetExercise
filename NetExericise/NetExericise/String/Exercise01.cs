c7d84477dfee3404a6cb25be30d747e71f4a10f0
Ho Huynh Nga <hnga3511@gmail.com>
2020-03-31 15:50:21 +0700

    Added string/exercise01

---
+++ NetExericise/NetExericise/String/Exercise01.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExericise.String
+{
+    public class Exercise01
+    {
+        public string TitleCaseToSnakeCase(string s)
+        {
+            return string.Concat(s.Select((x, i) =>
+                i > 0 && char.IsUpper(x) ? "_" + x.ToString() : x.ToString()))
+                .ToLower();
+        }
+    }
+}
