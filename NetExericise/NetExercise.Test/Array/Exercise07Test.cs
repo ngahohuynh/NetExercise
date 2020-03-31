05779a700afd87ba8836c04bd7d509315c381231
Ho Huynh Nga <hnga3511@gmail.com>
2020-03-31 13:43:12 +0700

    WIP

---
+++ NetExericise/NetExercise.Test/Array/Exercise07Test.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetExericise.Array;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExercise.Test.Array
+{
+    [TestClass]
+    public class Exercise07Test
+    {
+        [TestMethod]
+        public void Test_ChunkArray()
+        {
+            var exercise07 = new Exercise07();
+        }
+    }
+}
---
+++ NetExericise/NetExericise/Array/Exercise07.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExericise.Array
+{
+    public class Exercise07
+    {
+        public List<List<int>> ChunkArray(int[] arr, int n)
+        {
+            var result = new List<List<int>>();
+            for (var i = 0; i < (float)arr.Length / n; i++)
+            {
+                result.Add(arr.Skip(i * n).Take(n).ToList());
+            }
+
+            return result;
+        }
+    }
+}
