0fee4954312dd8e8faa839d776e59039bbc6745a
Ho Huynh Nga <hnga3511@gmail.com>
2020-04-01 14:13:30 +0700

    Added exercise02

---
+++ NetExericise/NetExericise/String/Exercise02.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExericise.String
+{
+    public class Exercise02
+    {
+        public string SnakeCaseToTitleCase(string s)
+        {
+            var sb = new StringBuilder();
+            sb.Append(char.ToUpper(s[0]));
+
+            for (var i = 1; i < s.Length; i++)
+            {
+                if (s[i] == '_')
+                {
+                    sb.Append(char.ToUpper(s[i + 1]));
+                    i++;
+                }
+                else
+                {
+                    sb.Append(s[i]);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
