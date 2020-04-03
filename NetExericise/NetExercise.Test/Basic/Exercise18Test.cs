2b911536c064204a24e2b43c712f1acc57185ebe
Ho Huynh Nga <hnga3511@gmail.com>
2020-04-03 09:48:55 +0700

    Added exercise18 basic

---
+++ NetExericise/NetExercise.Test/Basic/Exercise18Test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetExericise.Basic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExercise.Test.Basic
+{
+    [TestClass]
+    public class Exercise18Test
+    {
+        [TestMethod]
+        public void Test_IsHappyNumber()
+        {
+            var exercise18 = new Exercise18();
+            Assert.AreEqual(exercise18.IsHappyNumber(17), false);
+            Assert.AreEqual(exercise18.IsHappyNumber(19), true);
+        }
+    }
+}
