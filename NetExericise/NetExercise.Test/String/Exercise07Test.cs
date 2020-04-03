727325d7cabd63a1fc727bfbfad8541a583e7b14
Ho Huynh Nga <hnga3511@gmail.com>
2020-04-03 13:55:19 +0700

    Added exercise07 string

---
+++ NetExericise/NetExercise.Test/String/Exercise07Test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetExericise.String;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExercise.Test.String
+{
+    [TestClass]
+    public class Exercise07Test
+    {
+        [TestMethod]
+        public void Test_ConvertString()
+        {
+            var exercise07 = new Exercise07();
+            Assert.AreEqual(exercise07.ConvertString("abc4e6fd3"), "abcccceeeeeefddd");
+            Assert.AreEqual(exercise07.ConvertString("ab13c"), "abbbbbbbbbbbbbc");
+        }
+    }
+}
---
+++ NetExericise/NetExericise/String/Exercise07.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExericise.String
+{
+    public class Exercise07
+    {
+        public string ConvertString(string s)
+        {
+            var sb = new StringBuilder();
+            var len = 0;
+            var ch = '\0';
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (char.IsNumber(s[i]))
+                {
+                    len = len * 10 + (s[i] - '0');
+                    if (i == s.Length - 1 || !char.IsNumber(s[i + 1]))
+                    {
+                        for (var j = 1; j < len; j++)
+                        {
+                            sb.Append(ch);
+                        }
+                    }
+                }
+
+                else
+                {
+                    ch = s[i];
+                    sb.Append(ch);
+                    len = 0;
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
